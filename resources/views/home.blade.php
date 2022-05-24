518ae4c675b514ca4c435888c672cc1c4d955a6e
nischal adhikari <nish.adhikari111@gmail.com>
2022-05-24 11:55:38 +0545

    asdad

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,7 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+                    this is flight system
                 </div>
             </div>
         </div>
